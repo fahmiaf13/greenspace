6dc99ed0cdfd098cb597200e12179145ef3aef20
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-26 17:52:02 +0700

    feat: initial commit

---
+++ greenspace-be/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(
+  cors({
+    credentials: false,
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(morgan("combined"));
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
