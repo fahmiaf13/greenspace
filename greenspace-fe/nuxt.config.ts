477b73a163a6d7927c96c0233ab7d72d31e11235
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-28 20:32:24 +0700

    feat: built & config nuxt

--- greenspace-fe/nuxt.config.ts
+++ greenspace-fe/nuxt.config.ts
@@ -1,6 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  colorMode: { preference: "light" },
+  modules: ["@nuxtjs/google-fonts", "@nuxt/ui", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  css: ["~/assets/css/main.css"],
+  build: {
+    transpile: ["@vuepic/vue-datepicker"],
+  },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+      title: "Greenspace",
+    },
+  },
+
+  googleFonts: {
+    families: {
+      "Red+Hat+Text": {
+        wght: [300, 400, 500, 600, 700],
+      },
+      Blinker: {
+        wght: [300, 400, 500, 600, 700],
+      },
+    },
+  },
   devServer: {
     port: 3000,
   },
