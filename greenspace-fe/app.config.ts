46ef4f299f00174f96c7d07fa4d29ddf2c5591eb
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-29 13:33:23 +0700

    ui: add toast component

---
+++ greenspace-fe/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  ui: {
+    notifications: {
+      position: "bottom-0 right-0",
+    },
+    formGroup: {
+      help: "text-xs text-gray-500 dark:text-gray-400",
+      error: "text-xs mt-1 text-red-500 dark:text-red-400",
+    },
+  },
+});
