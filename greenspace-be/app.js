c6fa9de7a1c759243fcdd0dae20cfd8f2f708234
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-29 16:40:01 +0700

    chore: modify app and vercel.json

--- greenspace-be/app.js
+++ greenspace-be/app.js
@@ -22,12 +22,12 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Content-Type", "application/json;charset=UTF-8");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Content-Type", "application/json;charset=UTF-8");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(morgan("combined"));
 router(app);
