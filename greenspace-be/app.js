104e66c1f818bd0b30e1da5c51e9bdd17fa17f27
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-29 16:55:14 +0700

    fix: remove cores

--- greenspace-be/app.js
+++ greenspace-be/app.js
@@ -14,20 +14,24 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+//     optionsSuccessStatus: 200,
+//   })
+// );
 
-// app.use(function (req, res, next) {
-//   res.header("Content-Type", "application/json;charset=UTF-8");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader("content-type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Content-Type", "application/json;charset=UTF-8");
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(morgan("combined"));
 router(app);
