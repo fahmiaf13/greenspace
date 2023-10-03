0670f414807bcac776e152b35d00e7d0bb16a5c5
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-10-03 19:07:23 +0700

    fix: change url test

--- greenspace-fe/pages/index.vue
+++ greenspace-fe/pages/index.vue
@@ -37,7 +37,7 @@ const columns = [
 const fetchListParkingSpot = async () => {
   loading.table = true;
   try {
-    const response: Response<ParkingSpot[]> = await $fetch(`https://greenspace-be-git-dev-fahmiaf13.vercel.app/parking/spot`, { method: "GET", withCredentials: true, credentials: "include" });
+    const response: Response<ParkingSpot[]> = await $fetch(`${config.baseUrl}/parking/spot`, { method: "GET", withCredentials: true, credentials: "include" });
     listParkingSpot.value = response?.data;
   } catch (error) {
     console.error(error);
