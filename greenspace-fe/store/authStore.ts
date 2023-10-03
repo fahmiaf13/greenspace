b3a49818fe89d8ec1f8b3e6891f6aac6c8a9ee0a
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-10-03 21:09:28 +0700

    feat: add persist in store

--- greenspace-fe/store/authStore.ts
+++ greenspace-fe/store/authStore.ts
@@ -1,44 +1,48 @@
 import { defineStore } from "pinia";
 import { User, Response, Officer } from "~/types";
 
-export const useAuthStore = defineStore("auth", () => {
-  const config = useRuntimeConfig();
-  const token = ref<string | null | undefined>(null);
-  const member = ref<User | Officer | null>(null);
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    const config = useRuntimeConfig();
+    const token = ref<string | null | undefined>(null);
+    const member = ref<User | Officer | null>(null);
 
-  async function login(payload: { identifier: string; password: string; role: string }) {
-    if (payload.role === "USER") {
-      try {
-        const response: Response<User> = await $fetch(`${config.public.baseUrl}/auth/login/user`, { method: "POST", body: payload });
-        token.value = response.token;
-        member.value = response?.data as User;
-        return response;
-      } catch (error: any) {
-        return error;
+    async function login(payload: { identifier: string; password: string; role: string }) {
+      if (payload.role === "USER") {
+        try {
+          const response: Response<User> = await $fetch(`${config.public.baseUrl}/auth/login/user`, { method: "POST", body: payload });
+          token.value = response.token;
+          member.value = response?.data as User;
+          return response;
+        } catch (error: any) {
+          return error;
+        }
+      } else if (payload.role === "OFFICER") {
+        try {
+          const response: Response<Officer> = await $fetch(`${config.public.baseUrl}/auth/login/officer`, { method: "POST", body: payload });
+          token.value = response.token;
+          member.value = response?.data as Officer;
+          return response;
+        } catch (error: any) {
+          return error;
+        }
+      } else {
+        return { msg: "invalid role" };
       }
-    } else if (payload.role === "OFFICER") {
-      try {
-        const response: Response<Officer> = await $fetch(`${config.public.baseUrl}/auth/login/officer`, { method: "POST", body: payload });
-        token.value = response.token;
-        member.value = response?.data as Officer;
-        return response;
-      } catch (error: any) {
-        return error;
-      }
-    } else {
-      return { msg: "invalid role" };
     }
-  }
 
-  function logout() {
-    const auth = useCookie("auth");
-    auth.value = null;
+    function logout() {
+      const auth = useCookie("auth");
+      auth.value = null;
 
-    member.value = null;
-    token.value = null;
-  }
+      member.value = null;
+      token.value = null;
+    }
 
-  return { token, member, login, logout };
-});
+    return { token, member, login, logout };
+  },
+  { persist: true }
+);
 
 export type AuthStore = ReturnType<typeof useAuthStore>;
