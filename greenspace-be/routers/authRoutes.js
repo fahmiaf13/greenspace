d44d33045b1dd5dc91d5daed1c366df989c3d014
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-11-23 19:46:07 +0700

    pilkada

--- greenspace-be/routers/authRoutes.js
+++ greenspace-be/routers/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const pilkadaController = require("../controllers/pilkadaController/authPilkadaController");
+router.post("/login", pilkadaController.Login);
 
 router.post("/login/user", authController.loginAsUser);
 router.post("/login/officer", authController.loginAsOfficer);
